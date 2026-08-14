46e4d25041373163c1975591761465558ab913e6
Douglas Silva <sdouglas204@outlook.com>
2026-08-14 19:32:15 -0300

    Created paht and class

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.entities.CategoryEntity;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    private final CategoryService  categoryService;
+
+    public CategoryResource(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryEntity>> findAll() {
+        List<CategoryEntity> list =  categoryService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+}
